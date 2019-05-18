8c42d46b00c9e8e968654c87d4ade109de309f8d
Rui Borges <ruipedro.97@hotmail.com>
2019-05-18 23:01:57 +0100

    Added food endpoint

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -22,74 +22,100 @@ router.get('/listapis', function (req, res, next) {
   });
 });
 
-router.get('/getFoodUPC', (req, res, next) => {
-  if (req.query.upc) {
-    const upc = req.query.upc;
-    const client = new MongoClient(uri, {
-      useNewUrlParser: true
-    });
-    client.connect(err => {
-      if (err) throw err;
-      const collectionApis = client.db("ProjetoAMSSII").collection("Apis");
-      collectionApis.find({
-        type: "UPCSearch"
-      }).toArray((err, result) => {
-        if (err) {
-          throw err;
-        } else {
-          let urls = [];
-          var promises = [];
-          result.forEach(e => {
-            e.url = e.url.replace("${query}", upc);
-            console.log("request: " + e.url);
-            if (e.auth !== "none") {
-              promises.push(new Promise((resolve, reject) => {
-                var options = {
-                  method: "GET",
-                  url: e.url,
-                  headers: {
-                    'X-RapidAPI-Key': e.apikey
-                  }
-                };
-                request(options, function (error, response, body) {
-                  if (error) {
-                    reject(error);
-                  }
-                  if (!error && response.statusCode == 200) {
-                    resolve(body);
-                  } else if (response.statusCode == 404) {
-                    resolve(null);
-                  }
-                });
-              }));
 
-            } else {
-              promises.push(new Promise((resolve, reject) => {
-                request(e.url, function (error, response, body) {
-                  if (error) {
-                    reject(error);
-                  }
-                  if (!error && response.statusCode == 200) {
-                    resolve(JSON.parse(body));
-                  }
-                });
-              }));
-            }
-          });
+//i.e. http://localhost:3000/api/food/737628064502
+router.get('/food/:upc', (req, res, next) => {
+  var upc = req.params['upc'];
+  if (upc) {
 
-          Promise.all(promises).then(result => {
-            res.send(result);
-          }).catch(err => {
-            console.log(err);
-          });
+    request.get(`https://world.openfoodfacts.org/api/v0/product/${upc}.json`,(err,response,body)=>{
+    if(!err & response.statusCode === 200){
+      body = JSON.parse(body);
+      body =  {
+          status : body["status_verbose"],
+          sources : body.product.sources,
+          ingredients: body.product.ingredients,
+          nutrients_grade: body.product.nutrition_grades,
+          nutrients: body.product.nutriments
         };
-      });
-    })
+      res.send(body);
+    }
+    });
   } else {
-    res.status(400).send("No UPC provided");
+    res.statusCode(400).send("No UPC Provided");
   }
+});
+router.get('/food', (req, res, next) => {
+  res.send("Food endpoint");
 });
 
 
-
 module.exports = router;
+
+// router.get('/getFoodUPC', (req, res, next) => {
+//   if (req.query.upc) {
+//     const upc = req.query.upc;
+//     const client = new MongoClient(uri, {
+//       useNewUrlParser: true
+//     });
+//     client.connect(err => {
+//       if (err) throw err;
+//       const collectionApis = client.db("ProjetoAMSSII").collection("Apis");
+//       collectionApis.find({
+//         type: "UPCSearch"
+//       }).toArray((err, result) => {
+//         if (err) {
+//           throw err;
+//         } else {
+//           let urls = [];
+//           var promises = [];
+//           result.forEach(e => {
+//             e.url = e.url.replace("${query}", upc);
+//             console.log("request: " + e.url);
+//             if (e.auth !== "none") {
+//               promises.push(new Promise((resolve, reject) => {
+//                 var options = {
+//                   method: "GET",
+//                   url: e.url,
+//                   headers: {
+//                     'X-RapidAPI-Key': e.apikey
+//                   }
+//                 };
+//                 request(options, function (error, response, body) {
+//                   if (error) {
+//                     reject(error);
+//                   }
+//                   if (!error && response.statusCode == 200) {
+//                     resolve(body);
+//                   } else if (response.statusCode == 404) {
+//                     resolve(null);
+//                   }
+//                 });
+//               }));
+
+//             } else {
+//               promises.push(new Promise((resolve, reject) => {
+//                 request(e.url, function (error, response, body) {
+//                   if (error) {
+//                     reject(error);
+//                   }
+//                   if (!error && response.statusCode == 200) {
+//                     resolve(JSON.parse(body));
+//                   }
+//                 });
+//               }));
+//             }
+//           });
+
+//           Promise.all(promises).then(result => {
+//             res.send(result);
+//           }).catch(err => {
+//             console.log(err);
+//           });
+//         };
+//       });
+//     })
+//   } else {
+//     res.status(400).send("No UPC provided");
+//   }
+// });
