13ea9b7532bdde00ea12cde2746f36c9e4cc88d2
Rui Borges <ruipedro.97@hotmail.com>
2019-05-27 19:21:11 +0100

    V 0.3.0 - Added new routes

--- Backend/app.js
+++ Backend/app.js
@@ -3,15 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mustacheExpress = require('mustache-express');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api',apiRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -35,7 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
 });
 
 module.exports = app;
--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -27,95 +27,163 @@ router.get('/listapis', function (req, res, next) {
 router.get('/food/:upc', (req, res, next) => {
   var upc = req.params['upc'];
   if (upc) {
-
-    request.get(`https://world.openfoodfacts.org/api/v0/product/${upc}.json`,(err,response,body)=>{
-    if(!err & response.statusCode === 200){
-      body = JSON.parse(body);
-      body =  {
-          status : body["status_verbose"],
-          sources : body.product.sources,
+    request.get(`https://world.openfoodfacts.org/api/v0/product/${upc}.json`, (err, response, body) => {
+      if (!err & response.statusCode === 200) {
+        const client = new MongoClient(uri, {
+          useNewUrlParser: true
+        });
+        body = JSON.parse(body);
+        if (!body.product) {
+          res.send(500);
+          client.close();
+        }
+        body = {
+          status: body["status_verbose"],
+          sources: body.product.sources,
           ingredients: body.product.ingredients,
-          nutrients_grade: body.product.nutrition_grades,
-          nutrients: body.product.nutriments
+          nutritionGrade: body.product.nutrition_grades,
+          nutrients: body.product.nutriments,
+          brand: body.product.brands,
+          name: body.product["product_name"] !== "" ? body.product["product_name"] : body.product.generic_name,
+          upc: body.product.id
         };
-      res.send(body);
-    }
+
+        client.connect(err => {
+          if (err) throw err;
+          let collection = client.db("ProjetoAMSSII").collection("Products");
+          collection.findOne({
+            upc: body.upc
+          }, (err, result) => {
+            if (err) {
+              res.sendStatus(500).send("Damn, we found an error search for this product in our database");
+              client.close();
+            }
+            if (!result) {
+              collection.insertOne(body, (err, resp) => {
+                if (err) {
+                  res.sendStatus(500);
+                  client.close();
+                }
+                res.send(body);
+                client.close();
+              });
+            } else {
+              res.send(body);
+              client.close();
+            }
+          });
+        });
+
+      }
     });
   } else {
     res.statusCode(400).send("No UPC Provided");
+    client.close();
   }
 });
+
 router.get('/food', (req, res, next) => {
-  res.send("Food endpoint");
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true
+  });
+  client.connect(err => {
+    if (err) throw err;
+    let collection = client.db("ProjetoAMSSII").collection("Products");
+    collection.find({}).toArray().then(v => {
+      v = v.map(e => {
+        return {
+          upc: e.upc,
+          name: e.name,
+          grade: e.nutritionGrade ? e.nutritionGrade.toUpperCase() : "ND"
+        }
+      })
+      res.render('food', {
+        data: v
+      });
+    })
+  });
 });
 
+router.get("/populate", (req, res, next) => {
+
+  const clientRemote = new MongoClient(uri, {
+    useNewUrlParser: true
+  });
+  clientRemote.connect(err => {
+    if (err) throw err;
+    let collection = clientRemote.db("ProjetoAMSSII").collection("Products");
+
+    collection.remove({
+      name: null
+    }, result => {
+      res.send(result);
+    });
+
+  });
+})
+
+router.post("/foods", (req, res, next) => {
+  var upcs = req.body['upcs'];
+  if (upcs) {
+    upcs.forEach((element, i) => {
+      setTimeout(() => {
+        request.get(`http://localhost:3000/api/food/${element}`, (err, res, body) => {
+          if (err) {
+            console.log("Error");
+            return;
+          };
+          console.log(`[ADDED ${i}] ${res.statusCode}`);
+          return;
+        })
+      }, 500);
+    });
+    res.send(`Adding ${upcs.length} products`);
+  }
+});
+
+router.delete('/food/:upc', (req, res, next) => {
+  var upc = req.params['upc'];
+  if (upc) {
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true
+    });
+    client.connect(err => {
+      if (err) throw err;
+      let collection = client.db("ProjetoAMSSII").collection("Products");
+      collection.remove({upc:upc},(err,result)=>{
+        if(err) throw err;
+        res.redirect('/food');
+      });
+    });
+  }else{
+    res.send(400);
+  }
+
+});
+
+/**
+ * Método que calcula o indice saudável de um elemento, baseado nos seus dados nutricionais
+ * @param propotion Valor que indica o valor de referência do produto
+ * @param gorduraTotal  Valor total da gordura presente no alimento
+ * @param gorduraSat  Valor de gordura saturada do alimento
+ * @param gorduraTrans  Valor de gordura trans do alimento
+ * @param sodio Valor de sodio no alimento
+ * @param potassio Valor de potassio no alimento
+ * @param ferro Valor de ferro no alimento
+ * @param vitaminas Conjunto de vitaminas no alimento
+ * @param acuca Valor de acuçar presente no alimento
+ */
+function healthyRating(p, gorduraTotal, gorduraSat, gorduraTrans, sodio, potassio, acucar, acucarSat, ferro, vitaminas) {
+
+  var g = (((gorduraSat + gorduraTrans) / gorduraTotal) * gorduraTotal) / p; // Percentagem de gordura "má".
+  var s = sodio / p; // Percentagem de sódio -
+  var pt = potassio / p; // Percentagem de potassio +
+  var a = acucarSat / acucar //  Percentagem de açucar
+  var f = ferro / p; // Percentagem de ferro
+
+  if (gorduraTotal - (gorduraSat + gorduraTrans) < (gorduraSat + gorduraTrans)) {
+    return -1
+  }
+}
 
 module.exports = router;
-
-// router.get('/getFoodUPC', (req, res, next) => {
-//   if (req.query.upc) {
-//     const upc = req.query.upc;
-//     const client = new MongoClient(uri, {
-//       useNewUrlParser: true
-//     });
-//     client.connect(err => {
-//       if (err) throw err;
-//       const collectionApis = client.db("ProjetoAMSSII").collection("Apis");
-//       collectionApis.find({
-//         type: "UPCSearch"
-//       }).toArray((err, result) => {
-//         if (err) {
-//           throw err;
-//         } else {
-//           let urls = [];
-//           var promises = [];
-//           result.forEach(e => {
-//             e.url = e.url.replace("${query}", upc);
-//             console.log("request: " + e.url);
-//             if (e.auth !== "none") {
-//               promises.push(new Promise((resolve, reject) => {
-//                 var options = {
-//                   method: "GET",
-//                   url: e.url,
-//                   headers: {
-//                     'X-RapidAPI-Key': e.apikey
-//                   }
-//                 };
-//                 request(options, function (error, response, body) {
-//                   if (error) {
-//                     reject(error);
-//                   }
-//                   if (!error && response.statusCode == 200) {
-//                     resolve(body);
-//                   } else if (response.statusCode == 404) {
-//                     resolve(null);
-//                   }
-//                 });
-//               }));
-
-//             } else {
-//               promises.push(new Promise((resolve, reject) => {
-//                 request(e.url, function (error, response, body) {
-//                   if (error) {
-//                     reject(error);
-//                   }
-//                   if (!error && response.statusCode == 200) {
-//                     resolve(JSON.parse(body));
-//                   }
-//                 });
-//               }));
-//             }
-//           });
-
-//           Promise.all(promises).then(result => {
-//             res.send(result);
-//           }).catch(err => {
-//             console.log(err);
-//           });
-//         };
-//       });
-//     })
-//   } else {
-//     res.status(400).send("No UPC provided");
-//   }
-// });
