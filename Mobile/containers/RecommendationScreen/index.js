2d295a71fa4b8adf2863397fc2240f6f2ae57dd4
Rui Borges <rui.borges@estsetubal.ips.pt>
2019-06-21 17:40:30 +0100

    More recommendation things

--- Mobile/containers/RecommendationScreen/index.js
+++ Mobile/containers/RecommendationScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Spacer } from 'react-native';
+import { StyleSheet, View, Spacer, ImageBackground } from 'react-native';
 import { Header, Button, Icon, Input, Divider, ListItem, Text, Card, Image } from 'react-native-elements';
 import { connect } from 'react-redux';
 
@@ -36,32 +36,79 @@ class RecommendationScreen extends Component {
             <Card>
                 <View style={{ flexDirection: 'row' }}>
                     <Image source={{ uri: productData.img }}
-                        style={{ height: 75, width: 75, marginRight: 10, borderRadius:50 }}
+                        style={{ height: 75, width: 75, marginRight: 10, borderRadius: 50 }}
                     ></Image>
                     <View style={{ flexDirection: 'column' }}>
                         <Text h3>{productData.name}</Text>
                         <Text>
                             Code: {productData.upc}
-                    </Text>
+                        </Text>
                     </View>
                 </View>
             </Card>
             <Text h4>Recommendations:</Text>
             <View style={{ flex: 1, flexDirection: 'row' }}>
                 <View style={{ flex: 1, backgroundColor: "red", margin: 5 }}>
-                    <Text>P1</Text>
-
+                    <ImageBackground source={{ uri: "http://via.placeholder.com/300x300" }} style={{ width: '100%', height: '100%' }}>
+                        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+                            <View style={{ height: 65, padding: 5, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Text>P1</Text>
+                                <Image source={{ uri: productData.img }}
+                                    style={{
+                                        height: 45, width: 45, marginRight: 5, borderRadius: 30, borderWidth: 3,
+                                        borderColor: '#fff'
+                                    }}
+                                ></Image>
+                            </View>
+                        </View>
+                    </ImageBackground>
                 </View>
-                <View style={{ flex: 1, backgroundColor: "blue", margin: 5 }}>
-                    <Text>P2</Text>
+                <View style={{ flex: 1, backgroundColor: "red", margin: 5 }}>
+                    <ImageBackground source={{ uri: "http://via.placeholder.com/300x300" }} style={{ width: '100%', height: '100%' }}>
+                        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+                            <View style={{ height: 65, padding: 5, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Text>P2</Text>
+                                <Image source={{ uri: productData.img }}
+                                    style={{
+                                        height: 45, width: 45, marginRight: 5, borderRadius: 30, borderWidth: 3,
+                                        borderColor: '#fff'
+                                    }}
+                                ></Image>
+                            </View>
+                        </View>
+                    </ImageBackground>
                 </View>
             </View>
             <View style={{ flex: 1, flexDirection: 'row' }}>
-                <View style={{ flex: 1, backgroundColor: "blue", margin: 5 }}>
-                    <Text>P3</Text>
+                <View style={{ flex: 1, backgroundColor: "red", margin: 5 }}>
+                    <ImageBackground source={{ uri: "http://via.placeholder.com/300x300" }} style={{ width: '100%', height: '100%' }}>
+                        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+                            <View style={{ height: 65, padding: 5, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Text>P3</Text>
+                                <Image source={{ uri: productData.img }}
+                                    style={{
+                                        height: 45, width: 45, marginRight: 5, borderRadius: 30, borderWidth: 3,
+                                        borderColor: '#fff'
+                                    }}
+                                ></Image>
+                            </View>
+                        </View>
+                    </ImageBackground>
                 </View>
                 <View style={{ flex: 1, backgroundColor: "red", margin: 5 }}>
-                    <Text>P4</Text>
+                    <ImageBackground source={{ uri: "http://via.placeholder.com/300x300" }} style={{ width: '100%', height: '100%' }}>
+                        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+                            <View style={{ height: 65, padding: 5, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Text>P4</Text>
+                                <Image source={{ uri: productData.img }}
+                                    style={{
+                                        height: 45, width: 45, marginRight: 5, borderRadius: 30, borderWidth: 3,
+                                        borderColor: '#fff'
+                                    }}
+                                ></Image>
+                            </View>
+                        </View>
+                    </ImageBackground>
                 </View>
             </View>
         </View>
